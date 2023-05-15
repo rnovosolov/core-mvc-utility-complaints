1bc6bd52a3d574074a1448fd09f18f193d54ba7e
rnovo <r.novosolov@outlook.com>
2023-05-15 12:19:52 +0300

    Connect to azure db from localhost

--- UtilityComplaints.WebUI/Program.cs
+++ UtilityComplaints.WebUI/Program.cs
@@ -20,6 +20,10 @@ using Microsoft.Extensions.Localization;
 using Microsoft.AspNetCore.Mvc.Localization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Options;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using IdentityServer4.Models;
+using Microsoft.AspNetCore.Authentication.Google;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -31,7 +35,50 @@ builder.Configuration
 
 // Add services to the container.
 
-var connectionString = builder.Configuration["ConnectionStrings:DefaultConnection"];
+builder.Services.AddAzureAppConfiguration();
+
+
+// Install the Azure Key Vault and Azure Identity libraries
+// using NuGet: Microsoft.Azure.KeyVault and Azure.Identity
+
+// Create a new instance of the SecretClient using the Azure Identity library for authentication
+var keyVaultUri = new Uri("https://ucsecrets.vault.azure.net/");
+var credential = new DefaultAzureCredential();
+var secretClient = new SecretClient(keyVaultUri, credential);
+
+// Retrieve the secret values by name
+var clientIdSecret = await secretClient.GetSecretAsync("Authentication--Google--ClientId");
+var clientSecretSecret = await secretClient.GetSecretAsync("Authentication--Google--ClientSecret");
+var defaultConnectionSecret = await secretClient.GetSecretAsync("ConnectionStrings--AzureDefaultConnection");
+
+// Use the secret values to authenticate with the Google API
+var clientId = clientIdSecret.Value;
+var clientSecret = clientSecretSecret.Value;
+var defaultConnection = defaultConnectionSecret.Value;
+var connectionString = "";
+
+try
+{
+    connectionString = defaultConnection.Value.ToString();
+    //connectionString = Environment.GetEnvironmentVariable("AZURE_SQL_CONNECTIONSTRING");
+}
+catch
+{
+    // Failed to retrieve secret from Key Vault, try retrieving from local secrets
+    connectionString = builder.Configuration["ConnectionStrings:DefaultConnection"];
+}
+
+
+/*string connectionString = Environment.GetEnvironmentVariable("AZURE_SQL_CONNECTIONSTRING");
+
+if (string.IsNullOrEmpty(connectionString))
+{
+    // Connection string not found in environment variable, use value from appsettings.json
+    connectionString = builder.Configuration["ConnectionStrings:DefaultConnection"];
+}*/
+
+
+
 
 /*builder.Services.AddIdentityServer(options =>
     options.EmitStaticAudienceClaim = true
@@ -41,13 +88,25 @@ var connectionString = builder.Configuration["ConnectionStrings:DefaultConnectio
     .AddResourceStore<InMemoryResourcesStore>();*/
 
 
+
+
 builder.Services.AddAuthentication()
     .AddGoogle(googleOptions =>
     {
         //googleOptions.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
 
-        googleOptions.ClientId = builder.Configuration["Authentication:Google:ClientId"];
-        googleOptions.ClientSecret = builder.Configuration["Authentication:Google:ClientSecret"];
+        try
+        {
+            googleOptions.ClientId = clientId.Value.ToString();
+            googleOptions.ClientSecret = clientSecret.Value.ToString();
+        }
+        catch
+        {
+            // Failed to retrieve secret from Key Vault, try retrieving from local secrets
+            googleOptions.ClientId = builder.Configuration["Authentication:Google:ClientId"];
+            googleOptions.ClientSecret = builder.Configuration["Authentication:Google:ClientSecret"];
+        }
+
     });
 //add fb/github/linkedin
 
